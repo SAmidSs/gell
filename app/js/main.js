e320b232705d4f9f163e299da18e275d22740304
Dima Shkitin <amid2003s@gmail.com>
2021-10-26 18:37:44 +0300

    adaptive blog-one, product, product-list, product-detalis

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,51 @@
 $(function () {
 
 
-    $('.menu__btn').on('click', function(){
+
+    $('.menu__btn').on('click', function () {
         $('.menu__list-items').toggleClass('menu__list-items--active');
     });
+
+    $('.shop__filter-btn').on('click', function () {
+        $('.shop__filter').toggle({
+            duration: 500,
+        });
+    });
+
 
     $('.product-slider__items').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         infinite: false,
         prevArrow: '<button type="button" class="slick-next"><svg width="22px" height="9px"><path fill-rule="evenodd"  fill="rgb(163, 187, 200)"d="M21.704,3.526 L21.712,3.529 L4.756,3.529 L4.856,0.670 C4.910,0.616 4.940,0.542 4.940,0.465 C4.940,0.388 4.910,0.315 4.856,0.261 L4.684,0.087 C4.629,0.033 4.557,0.003 4.480,0.003 C4.403,0.003 4.331,0.033 4.276,0.087 L0.084,4.298 C0.030,4.353 -0.000,4.426 -0.000,4.503 C-0.000,4.580 0.030,4.653 0.084,4.707 L4.276,8.920 C4.331,8.973 4.403,9.003 4.480,9.003 C4.557,9.003 4.629,8.973 4.684,8.920 L4.856,8.746 C4.910,8.692 4.940,8.619 4.940,8.542 C4.940,8.465 4.910,8.396 4.856,8.342 L4.743,5.478 L21.708,5.478 C21.867,5.478 22.000,5.340 22.000,5.181 L22.000,3.812 C22.000,3.653 21.863,3.526 21.704,3.526 Z"/></svg></button>',
-        
+
         nextArrow: '<button type="button" class="slick-prev"><svg width="22px" height="9px"><path fill-rule="evenodd"  fill="rgb(163, 187, 200)"d="M0.296,5.473 L0.288,5.471 L17.244,5.471 L17.144,8.329 C17.089,8.384 17.060,8.458 17.060,8.534 C17.060,8.612 17.089,8.684 17.144,8.739 L17.316,8.912 C17.371,8.967 17.443,8.996 17.520,8.996 C17.597,8.996 17.669,8.967 17.724,8.913 L21.916,4.701 C21.970,4.647 22.000,4.573 22.000,4.497 C22.000,4.420 21.970,4.347 21.916,4.293 L17.724,0.080 C17.669,0.026 17.597,-0.003 17.520,-0.003 C17.443,-0.003 17.371,0.026 17.316,0.080 L17.144,0.254 C17.089,0.307 17.060,0.380 17.060,0.458 C17.060,0.535 17.089,0.604 17.144,0.657 L17.257,3.522 L0.292,3.522 C0.133,3.522 -0.000,3.659 -0.000,3.818 L-0.000,5.188 C-0.000,5.347 0.137,5.473 0.296,5.473 Z"/></svg></button>',
+        responsive: [
+            {
+                breakpoint: 1140,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 860,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 591,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
-    $('.product-tabs__top-item').on('click', function(e){
+    $('.product-tabs__top-item').on('click', function (e) {
         e.preventDefault();
         $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
         $(this).addClass('product-tabs__top-item--active');
@@ -49,12 +80,12 @@ $(function () {
     //     if (e.target.classList.contains('product-item__btn--active')) alert(e.target.classList.contains('product__item-btn__items--active'))
     //   })
 
-    $('.product-item__btn').on('click', function(){
+    $('.product-item__btn').on('click', function () {
         $(this).addClass('product-item__btn--noactive');
         $($('.product__item-box').removeClass('product__item-box--noactive'));
     });
 
-    $('.product__item-box__link').on('click', function(){
+    $('.product__item-box__link').on('click', function () {
         $(this).addClass('product__item-box__link--noactive');
         $(this).removeClass('product__item-box__link--noactive');
         $($('.product-item__btn').removeClass('product-item__btn--noactive'));
@@ -106,8 +137,8 @@ $(function () {
         arrows: false,
         infinite: false,
         slidesToShow: 1,
-    });    
-    
+    });
+
     var containerEl1 = document.querySelector('[data-ref="mix-1"]');
     var containerEl2 = document.querySelector('[data-ref="mix-2"]');
 
@@ -122,6 +153,6 @@ $(function () {
 
 
 
-    
+
 
 })
